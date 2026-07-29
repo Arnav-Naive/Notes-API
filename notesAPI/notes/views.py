c478b166fbb3762a70857cfc1913b68b0a8a09f4
Arnav-Naive <arnavfating09@gmail.com>
2026-07-30 01:24:37 +0530

    Add Note model, serializer, view, and URL routing - GET working

--- notesAPI/notes/views.py
+++ notesAPI/notes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Note
+from .serializers import NoteSerializer
+
+class NoteListCreate(generics.ListCreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
